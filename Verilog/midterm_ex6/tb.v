9c8e7f66808e722dc2e430ba34a1f3c9d221fad9
Rex3388 <zz531211@gmail.com>
2025-10-31 18:15:27 +0800

    fix: solve the prob that can't not yield the wave
    Solution: $iverilog -o file.v tb.v -> delete -o and type, then we have
    file "a.out", type in terminal "$vvp a.out", done

--- Verilog/midterm_ex6/tb.v
+++ Verilog/midterm_ex6/tb.v
@@ -3,41 +3,27 @@ reg x,y,w,z;
 wire f;
 circuit modulee(.f(f), .x(x), .y(y), .w(w), .z(z));
 initial begin
-  x=0;y=0;w=0;z=0;
-  #10;
-  x=0;y=0;w=0;z=1;
-  #10;
-  x=0;y=0;w=1;z=0;
-  #10;
-  x=0;y=0;w=1;z=1;
-  #10;
-  x=0;y=1;w=0;z=0;
-  #10;
-  x=0;y=1;w=0;z=1;
-  #10;
-  x=0;y=1;w=1;z=0;
-  #10;
-  x=0;y=1;w=1;z=1;
-  #10;
-  x=1;y=0;w=0;z=0;
-  #10;
-  x=1;y=0;w=0;z=1;
-  #10;
-  x=1;y=0;w=1;z=0;
-  #10;
-  x=1;y=0;w=1;z=1;
-  #10;
-  x=1;y=1;w=0;z=0;
-  #10;
-  x=1;y=1;w=0;z=1;
-  #10;
-  x=1;y=1;w=1;z=0;
-  #10;
-  x=1;y=1;w=1;z=1;
-  #10;
+    for(integer i=0;i<=1;i=i+1)
+    begin
+        for(integer j=0;j<=1;j=j+1)
+        begin
+            for(integer k=0;k<=1;k=k+1)
+            begin
+                for(integer l=0;l<=1;l=l+1)
+                begin
+                    x=i;
+                    y=j;
+                    w=k;
+                    z=l;
+                    #10;
+                end
+            end
+        end
+    end
+    $finish;
 end
 initial begin
-    $dumpfile("wave.vcd");//  iverilog -o circuit.v tb.v
+    $dumpfile("wave.vcd");//  iverilog circuit.v tb.v
     $dumpvars(0, tb); // vvp wave
 end
 endmodule
