286800d98b282a424e1e6e97c4c977a99ed8686a
Rex3388 <zz531211@gmail.com>
2026-03-30 21:53:14 +0800

    feature: change file name "2022IC" to "IC_2022"

---
+++ Verilog/IC_2021/geofence.v
@@ -0,0 +1,12 @@
+module geofence ( clk,reset,X,Y,valid,is_inside);
+input clk;
+input reset;
+input [9:0] X;
+input [9:0] Y;
+output valid;
+output is_inside;
+//reg valid;
+//reg is_inside;
+
+endmodule
+
